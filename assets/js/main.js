5cdd13900260cab1fff2998218bdf1881fd3ea22
phunguyen4869 <phunguyen4869@gmail.com>
2022-03-16 12:05:16 +0700

    fix cart hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
-$('.cart-icon').hover(function(){
+$('.cart-icon').hover(function () {
     $('.cart_list').css('display', 'flex');
-}, function(){
+    $('.cart_list').hover(function () {
+        $('.cart_list').css('display', 'flex');
+    }, function () {
+        $('.cart_list').css('display', 'none');
+    });
+}, function () {
     $('.cart_list').css('display', 'none');
 });
